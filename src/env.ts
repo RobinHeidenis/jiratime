6a52ec9343aa59988527c67755dde09454f145a3
Robin Heidenis <robin.heidenis@gmail.com>
2025-02-16 23:46:28 +0000

    feat: support loading environment variables through config file

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,27 @@
+import { readFileSync } from "node:fs";
+import { homedir } from "node:os";
+import path from "node:path";
 import { z } from "zod";
+
+const CONFIG_LOCATION = path.join(homedir(), ".config", "jira-tui.json");
+
+let configFileConfig: object | undefined;
+try {
+  const configFile = readFileSync(CONFIG_LOCATION, "utf-8");
+  configFileConfig = JSON.parse(configFile);
+} catch {
+  console.error("❌ Missing or invalid config file");
+}
 
 const envVariables = z.object({
   JIRA_API_KEY: z.string(),
   JIRA_BASE_URL: z.string().url(),
-  JIRA_BOARD_ID: z.string(),
+  JIRA_BOARD_ID: z.string().or(z.number()),
   STORY_POINTS_FIELD: z.string(),
 });
 
-const result = envVariables.safeParse(process.env);
+const mergedConfig = { ...configFileConfig, ...process.env };
+const result = envVariables.safeParse(mergedConfig);
 
 if (!result.success) {
   console.error("❌ Missing or invalid environment variables:");
